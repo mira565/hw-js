de7d85529c96f134e395d228534af9384217e8b9
mira <miratulebaeva@gmail.com>
2022-05-30 16:47:56 +0600

    user name ILya and other 3 alerts for task 3 were added

--- index.js
+++ index.js
@@ -16,88 +16,10 @@ console.log(user)
 
 let userName = "Ilya";
 console.log(userName)
-    
+
 alert(`hello ${1}`); // ?
-alert( `hello ${"name"}` ); // ?
-alert( `hello ${name}` ); // ?
-
-let userID=prompt('Enter your name','')
-console.log("" + 1 + 0)
-console.log("" - 1 + 0)
-console.log(6 / "3")
-console.log("2" * "3")
-console.log(4 + 5 + "px")
-console.log("$" + 4 + 5)
-console.log("4" - 2)
-console.log("4px" - 2)
-console.log("-9" + 5)
-console.log("-9" - 5)
-console.log(null + 1)
-console.log(undefined + 1)
-console.log(" \t \n" - 2) 
-let A = +prompt("Первое число?", 1);
-let B = +prompt("Второе число?", 2);
-alert(a+b);
-
-let num='3';
-alert(num)
-
-
-// Создайте переменные a=10 и b=2. Выведите на экран их сумму, разность, произведение и частное (результат деления).
-
-let a='10';
-let b='2';
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-
-
-// Создайте переменные c=15 и d=2. Просуммируйте их, а результат присвойте переменной result. Выведите на экран значение переменной result.
-
-let c=+prompt("c", 15);
-let d=+prompt("d", 2);
-let result=c+d;
-alert(result)
-
-//Создайте переменные a=10, b=2 и c=5. Выведите на экран их сумму.
-let a1='10';
-let b1='2';
-let c1='5';
-console.log(a1+b1+c1)
-
-
-// Создайте переменные a=17 и b=10. Отнимите от a переменную b и результат присвойте переменной c. Затем создайте переменную d, присвойте ей значение 7. Сложите переменные c и d, а результат запишите в переменную result. Выведите на экран значение переменной result.
-let a2=17;
-let b2=10;
-let c2=a2-b2;
-console.log(a2-b2)
-let d2=7;
-let result3=c2+d2;
-alert(result3)
-
-// Создайте переменную, присвойте ей число. Возведите это число в квадрат. Выведите его на экран.
-let a3=5
-console.log(a3**2)
-
-//Напишите скрипт, который считает количество секунд в часе, в сутках, в месяце.
-let minute=60
-let hour=60
-let result4=60*60
-alert(result4)
-
-let day=24
-let result5=minute*hour*day
-alert(result5)
-
-
-let month=31
-let result6=result5*31
-alert(result6)
-
-
-
-
+alert( `hello ${"userName"}` ); // ?
+alert( `hello ${userName}` ); // ?
 
 
 
